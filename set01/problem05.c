db9c92e7752aa938276f9c6232cf1a065cf2ff86
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-10-12 18:13:26 +0530

    committed 10/12/2023, 6:13:26 PM

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,36 @@
-
+//Program to compare  three numbers using pass by value
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("Enter the number: ");
+  scanf("%d",&a);
+  return a;
+}
+int compare(int a,int b,int c)
+{
+  if(a>b&&a>c)
+  {
+    return a;
+  }else if(b>c)
+  {
+    return b;
+  }else
+  {
+    return c;
+  }
+}
+void output(int a,int b,int c,int largest)
+{
+  printf("The largest of %d,%d and %d is %d",a,b,c,largest);
+}
+int main()
+{
+  int x,y,z,large;
+  x=input();
+  y=input();
+  z=input();
+  large=compare(x,y,z);
+  output(x,y,z,large);
+  return 0;
+}
