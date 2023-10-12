621366854cd543bdac721696409624e0cec5bed7
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-10-12 16:17:11 +0530

    committed 10/12/2023, 4:17:11 PM

---
+++ set02/problem02.c
@@ -0,0 +1,41 @@
+//program to find if a trangle is scalene
+#include<stdio.h>
+int input_side()
+{
+  int a;
+  printf("Enter the side of the triangle: ");
+  scanf("%d",&a);
+  return a;
+}
+int check_scalene(int a,int b,int c)
+{
+  if(a!=b&&a!=c)
+  {
+    if(b!=c){
+      return 1;
+    }
+  }else{
+    return 0;
+  }
+  
+}
+void output(int a,int b,int c,int isscalene)
+{
+  if(isscalene!=0)
+  {
+    printf("The triangle with sides %d, %d and %d is a scalene.",a,b,c);
+
+  }else{
+    printf("The triangle with sides %d,%d and %d is not scalene. ",a,b,c);
+  }
+}
+int main()
+{
+  int x,y,z, isscalene;
+  x=input_side();
+  y=input_side();
+  z=input_side();
+  isscalene=check_scalene(x,y,z);
+  output(x,y,z,isscalene);
+  return 0;
+}
