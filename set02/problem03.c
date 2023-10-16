cea5bd1bee11ee3ed863d66370233e2b3a580594
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-10-16 09:34:49 +0530

    committed 10/16/2023, 9:34:49 AM

--- set02/problem03.c
+++ set02/problem03.c
@@ -1,1 +1,43 @@
-//
+//Program to find whether a number is composite number 
+#include<stdio.h>
+int input_number()
+{
+  int n;
+  printf("Enter the number:");
+  scanf("%d",&n);
+  return n;
+}
+int is_composite(int n)
+{
+  if(n>1)
+  {
+    for(int i=2;i<=n/2;i++)
+    {
+      if(n%i==0)
+      {
+        return 1;
+      }
+    }
+   
+}
+ return 0;
+}
+  
+
+void output(int n,int result)
+{
+  if(result==1)
+  {
+    printf("%d is a composite number",n);
+  }else{
+    printf("%d is not a composite number",n);
+  }
+}
+int main()
+{
+  int n,result;
+  n=input_number();
+  result=is_composite(n);
+  output(n,result);
+  return 0;
+}
