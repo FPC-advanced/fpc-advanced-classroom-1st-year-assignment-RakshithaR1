2e266e6c665c7a16f12a6e57e49ada8865c436e8
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-10-16 10:06:20 +0530

    committed 10/16/2023, 10:06:20 AM

---
+++ set02/problem04.c
@@ -0,0 +1,14 @@
+//Program to find the sum of composite number in an array of different 
+//numbers entered by the user
+#include<stdio.h>
+int input_array_size()
+{
+  int n;
+  printf("Enter the size of the array: ");
+  scanf("%d",&n);
+  return n;
+}
+void input_array(int n,int a[n])
+{
+  printf("Enter the ")
+}
