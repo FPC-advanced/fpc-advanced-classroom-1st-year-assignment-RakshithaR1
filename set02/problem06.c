7468719b35e61ba682f4ba0cbd24538f770a90b1
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-10-17 11:27:35 +0530

    committed 10/17/2023, 11:27:34 AM

---
+++ set02/problem06.c
@@ -0,0 +1,33 @@
+//Program to reverse a string 
+#include<stdio.h>
+void input_string(char *a)
+{
+  printf("Enter the string: ");
+  scanf("%s",a);
+}
+void str_reverse(char *str,char *rev_str)
+{  
+  int n=0;
+  while(str[n]!='\0')
+  {
+     n++;
+  }
+  for(int i=0;i<n;i++)
+  {
+    rev_str[i]=str[n-i-1];
+  }
+  rev_str[n]='\0';
+}
+void output(char *a,char *reverse_a)
+{
+  printf("The Original string is %s \n",a);
+  printf("The reversed string is %s \n",reverse_a);
+}
+int main()
+{
+  char s[100],rev[100];
+  input_string(s);
+  str_reverse(s,rev);
+  output(s,rev);
+  return 0;
+}
