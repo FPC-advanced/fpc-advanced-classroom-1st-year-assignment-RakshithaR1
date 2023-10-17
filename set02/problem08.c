77c2be8504c24d1f37bf8301d53a7ae8831bb761
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-10-17 19:43:05 +0530

    committed 10/17/2023, 7:43:04 PM

--- set02/problem08.c
+++ set02/problem08.c
@@ -12,3 +12,60 @@ int input_n()
   scanf("%d",&n);
   return n;
 }
+Triangle input_triangle()
+{
+  Triangle t;
+  printf("Enter the base and altitude of the triangle: ");
+  scanf("%f %f",&t.base,&t.altitude);
+  return t;
+}
+void input_n_triangles(int n,Triangle t[n])
+{
+  for(int i=0;i<n;i++)
+  { 
+    printf("%d Triangle:\n",i+1);
+    t[i]=input_triangle();
+  }
+}
+void find_area(Triangle *t)
+{
+  t->area=0.5*t->altitude*t->base;
+}
+void find_n_areas(int n,Triangle t[n])
+{
+  for(int i=0;i<n;i++)
+  {
+    find_area(&t[i]);
+  }
+}
+Triangle find_smallest_triangle(int n,Triangle t[n])
+{
+   for(int i=0;i<n;i++)
+   {
+    if(t[i].area<t[i+1].area)
+    {
+      return t[i];
+    }
+   }
+}
+void output(int n,Triangle t[n],Triangle smallest)
+{
+  printf("The smallest triangle out of %d triangles with base and height ",n);
+  for(int i=0;i<n;i++)
+  {
+    printf("(%.0f, %.0f)",t[i].base,t[i].altitude);
+  }
+  printf("is the triangle having base %.2f,height %.2f and area %.2f",smallest.base,smallest.altitude,smallest.area);
+}
+int main()
+{
+  int n;
+  Triangle t[100],smallest;
+  n=input_n();
+  input_n_triangles(n,t);
+  find_n_areas(n,t);
+  smallest=find_smallest_triangle(n,t);
+  output(n,t,smallest);
+  return 0;
+}
+
