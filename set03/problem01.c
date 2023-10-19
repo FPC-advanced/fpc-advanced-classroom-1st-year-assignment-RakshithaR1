d8489a39719d6fca964f9e5d21af0e6f16711bf1
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-10-19 16:16:29 +0530

    committed 10/19/2023, 4:16:28 PM

--- set03/problem01.c
+++ set03/problem01.c
@@ -13,5 +13,7 @@ float find_distance(float x1,float y1,float x2,float y2)
   float dx=x1-y1;
   float dy=y2-y1;
   float sqr_dist=(dx*dx)+(dy*dy);
-  do
+  float guess=sqr_dist;
+  float pre_guess;
+  float epsilon
 }
