25d57724c9981b0ee4e841e43ff8085540f8414d
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-10-19 16:09:59 +0530

    committed 10/19/2023, 4:09:58 PM

--- set03/problem01.c
+++ set03/problem01.c
@@ -1,4 +1,7 @@
 //Program to find the distance between two points 
 #include<stdio.h>
 #include<math.h>
-void input(float *x1,float *y1,float *x2,float *x3)
+void input(float *x1,float *y1,float *x2,float *y2)
+{
+  printf("Enter the co-ordinates of the 1st point: ")
+}
