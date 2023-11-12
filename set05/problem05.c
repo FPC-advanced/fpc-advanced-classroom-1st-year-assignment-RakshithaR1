68bf2de981799dd125126aba95a4a67f1deba402
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-11-12 22:22:38 +0530

    committed 11/12/2023, 10:22:37 PM

---
+++ set05/problem05.c
@@ -0,0 +1,16 @@
+//Program to find borga(x) given x
+#include<stdio.h>
+int input(int x)
+{
+  printf("Enter the value of x: ");
+  scanf("%d",&x);
+  return x;
+}
+float borga_x(int x)
+{
+
+}
+void output(int x,float result)
+{
+  
+}
---
+++ set05/problem06.c
@@ -0,0 +1,45 @@
+//Program to find the average of all the odd elements in an array
+#include<stdio.h>
+int input_n()
+{
+  int n;
+  printf("Enter the size of the array: ");
+  scanf("%d",&n);
+  return n;
+}
+void input(int n,int a[n])
+{
+  printf("Enter the elements of the array:\n ");
+  for(int i=1;i<=n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+}
+float odd_average(int n,int a[n])
+{
+   float sum=0.0;
+   int counts=0;
+  for(int i=1;i<=n;i++)
+  {
+    if(i%2!=0)
+    {
+      sum += a[i];
+      counts +=1;
+    }
+  }
+  return sum/counts;
+}
+void output(float avg)
+{
+  printf("Average of all the odd elements is: %f",avg);
+}
+int main()
+{
+  int n,a[100];
+  float avg;
+  n=input_n();
+  input(n,a);
+  avg=odd_average(n,a);
+  output(avg);
+  return 0;
+}
