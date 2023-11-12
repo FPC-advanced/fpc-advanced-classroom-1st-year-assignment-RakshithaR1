5fdb597530c22efa0a5024c3e04aa156f5b9274d
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-11-12 19:18:44 +0530

    committed 11/12/2023, 7:18:43 PM

---
+++ set05/problem01.c
@@ -0,0 +1,44 @@
+// Program to find the distance between two points
+struct _point
+{
+  float x;
+  float y;
+};
+typedef struct _point Point;
+Point input()
+{
+  Point a;
+  printf("Enter the x co-ordinate: ");
+  scanf("%f", &a.x);
+  printf("Enter the y co-ordinate: ");
+  scanf("%f", &a.y);
+  return a;
+}
+void dist(Point a, Point b, float *res)
+{
+  float c, d, sqr;
+  c = b.x - a.x;
+  d = b.y - a.y;
+  sqr = (c * c) + (d * d);
+  float guess = sqr, preguess;
+  while (fabs(guess - preguess) > 0.0001)
+  {
+    preguess = guess;
+    guess = 0.5 * (guess + sqr / guess);
+  }
+  *res = guess;
+}
+void output(Point a, Point b, float res)
+{
+  printf("The Distance between (%.1f,%.1f) and (%.1f,%.1f) is %.1f", a.x, a.y, b.x, b.y, res);
+}
+int main()
+{
+  Point x, y;
+  float dista;
+  x = input();
+  y = input();
+  dist(x, y, &dista);
+  output(x, y, dista);
+  return 0;
+}
