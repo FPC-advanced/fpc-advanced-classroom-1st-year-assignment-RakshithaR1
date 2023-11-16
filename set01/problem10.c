18ab20cd7ede16c618944c590111ebc7d56dd24d
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-11-16 17:45:35 +0530

    committed 11/16/2023, 5:45:33 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -10,14 +10,8 @@ void input_two_strings(char *string1, char *string2)
 
 int stringcompare(char *string1, char *string2)
 {
-  int j=0;
- for(int i=0;string1[i]==string2[i]&&string1[i]!='\0'||string2[i]!='\0';i++)
- {
-  j=i;
-  i++;
-  
- }
- return string1[j]-string2[j];
+  int i=0;
+  while(string1[i]==string2[i]&&string1[i])
 }
 
 void output(char *string1, char *string2, int result)
