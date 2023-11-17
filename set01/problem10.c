b3e1dc832ee62cb8282547eded7ffac515dbc833
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-11-17 14:51:32 +0530

    committed 11/17/2023, 2:51:31 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -13,7 +13,7 @@ int stringcompare(char *string1, char *string2)
    int i=0;
    while(string1[i]==string2[i])
    {
-     if(string1[i]=='\0')
+     if(string1[i]=='\0'||string2[i]=='\0')
    }
   
  
