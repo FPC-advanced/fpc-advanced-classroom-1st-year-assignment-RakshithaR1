47eaf71edb31469e2bf168875ca3715f1dd2806d
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-11-21 17:35:42 +0530

    committed 11/21/2023, 5:35:40 PM

--- set03/problem03.c
+++ set03/problem03.c
@@ -1,42 +1,44 @@
-//Program to check if the given  number is prime
-#include<stdio.h>
+// Program to check if the given  number is prime
+#include <stdio.h>
 int input_number()
 {
   int n;
   printf("Enter the number: ");
-  scanf("%d",&n);
+  scanf("%d", &n);
   return n;
 }
 int is_prime(int n)
 {
-  if(n<=1)
+  if (n <= 1)
   {
     return 0;
   }
-    for(int i=2;i<n;i++)
-    {
-      if(n%i==0)
-      {
-        return 0;
-      }
-    }
-    return 1;
-  }
-
-void output(int n,int result)
-{
-  if(result!=0)
+  for (int i = 2; i < n; i++)
   {
-    printf("%d is a prime number",n);
-  }else{
-    printf("%d is not a prime number",n);
+    if (n % i == 0)
+    {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+void output(int n, int result)
+{
+  if (result != 0)
+  {
+    printf("%d is a prime number", n);
+  }
+  else
+  {
+    printf("%d is not a prime number", n);
   }
 }
 int main()
 {
-  int n,prime;
-  n=input_number();
-  prime=is_prime(n);
-  output(n,prime);
+  int n, prime;
+  n = input_number();
+  prime = is_prime(n);
+  output(n, prime);
   return 0;
 }
