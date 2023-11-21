674d8b56833b6cefdf0bdc752a88412b0124e45f
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-11-21 17:52:12 +0530

    committed 11/21/2023, 5:52:11 PM

--- set03/problem05.c
+++ set03/problem05.c
@@ -1,55 +1,54 @@
-//Program to find all the prime numbers between 2 to n Eratosthenes Sieve method
-#include<stdio.h>
+// Program to find all the prime numbers between 2 to n Eratosthenes Sieve method
+#include <stdio.h>
 int input_array_size()
 {
   int n;
   printf("Enter the size of the array: ");
-  scanf("%d",&n);
+  scanf("%d", &n);
   return n;
 }
-void init_array(int n,int a[n])
+void init_array(int n, int a[n])
 {
-  for(int i=2;i<=n;i++)
+  for (int i = 2; i <= n; i++)
   {
-    a[i]=1;
+    a[i] = 1;
   }
 }
-void erotosthenes_sieve(int n,int a[n])
+void erotosthenes_sieve(int n, int a[n])
 {
-   for(int i=2;i*i<=n;i++)
-   {
-    if(a[i]==1)
+  for (int i = 2; i * i <= n; i++)
+  {
+    if (a[i] == 1)
     {
-      for(int j=i*i;j<=n;j+=i)
+      for (int j = i * i; j <= n; j += i)
       {
-        a[j]=0;
+        a[j] = 0;
       }
     }
-   }
-
-}
-void output(int n,int a[n])
-{
- printf("The prime numbers between 2 to %d is ",n);
- for(int i=2;i<=n;i++)
- {
-  if(a[i]==1)
-  {
-    printf("%d",i);
-   while(i<n)
-   {
-    printf("  ");
-    break;
-   }
   }
- }
+}
+void output(int n, int a[n])
+{
+  printf("The prime numbers between 2 to %d is ", n);
+  for (int i = 2; i <= n; i++)
+  {
+    if (a[i] == 1)
+    {
+      printf("%d", i);
+      while (i < n)
+      {
+        printf("  ");
+        break;
+      }
+    }
+  }
 }
 int main()
 {
-  int n,a[100];
-  n=input_array_size();
-  init_array(n,a);
-  erotosthenes_sieve(n,a);
-  output(n,a);
+  int n, a[100];
+  n = input_array_size();
+  init_array(n, a);
+  erotosthenes_sieve(n, a);
+  output(n, a);
   return 0;
 }
