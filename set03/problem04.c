ece041c8e5d7e668756f8f3b1df82f585f54b25a
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-11-21 17:40:12 +0530

    committed 11/21/2023, 5:40:11 PM

--- set03/problem04.c
+++ set03/problem04.c
@@ -1,36 +1,36 @@
-//Program to find nth number in the fibonacci sequence.
-#include<stdio.h>
+// Program to find nth number in the fibonacci sequence.
+#include <stdio.h>
 int input()
 {
   int n;
   printf("Enter the number: ");
-  scanf("%d",&n);
+  scanf("%d", &n);
   return n;
 }
 int find_fibo(int n)
 {
-  int a=0,b=1,c;
-  if(n==a)
+  int a = 0, b = 1, c;
+  if (n == a)
   {
     return a;
   }
-  for(int i=2;i<=n;i++)
+  for (int i = 2; i <= n; i++)
   {
-    c=a+b;
-    a=b;
-    b=c;
+    c = a + b;
+    a = b;
+    b = c;
   }
   return b;
 }
-void output(int n,int fibo)
+void output(int n, int fibo)
 {
-  printf("fibo(%d)=%d",n,fibo);
+  printf("fibo(%d)=%d", n, fibo);
 }
 int main()
 {
-  int n,fibo;
-  n=input();
-  fibo=find_fibo(n);
-  output(n,fibo);
+  int n, fibo;
+  n = input();
+  fibo = find_fibo(n);
+  output(n, fibo);
   return 0;
 }
