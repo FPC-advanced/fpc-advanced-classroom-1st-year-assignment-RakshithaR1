3c490e19bf1669390dea4372941c723345b1ff08
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-11-22 20:45:38 +0530

    committed 11/22/2023, 8:45:36 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -13,7 +13,7 @@ void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float
 int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3)
 {
   float area;
-  area = 0.5 * fabs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+  area = 0.5 * fabs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)));
   if (area < 0.000001)
   {
     return 0;
