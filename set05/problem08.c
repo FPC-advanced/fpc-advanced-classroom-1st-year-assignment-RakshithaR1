4c9511a9b9a370a1ee78bc795b1a2109ba937a42
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-11-27 10:30:32 +0530

    committed 11/27/2023, 10:30:31 AM

--- set05/problem08.c
+++ set05/problem08.c
@@ -1,64 +1,64 @@
-//Program to find the weight of a truck  load of n camels
-#include<stdio.h>
-#include<math.h>
+// Program to find the weight of a truck  load of n camels
+#include <stdio.h>
+#include <math.h>
 typedef struct _camel
 {
-  float radius,height,length,weight;
-}Camel;
-void input(int n,Camel c[n],float *truck_weight)
+  float radius, height, length, weight;
+} Camel;
+void input(int n, Camel c[n], float *truck_weight)
 {
   printf("Enter the number of camels: \n");
-  scanf("%d",&n);
-  for(int i=0;i<n;i++)
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++)
   {
-    printf("Enter the height of camel no. %d:\n",i+1);
-    scanf("%f",&c[i].height);
-    printf("Enter the length of camel no. %d:\n",i+1);
-    scanf("%f",&c[i].length);
-    printf("Enter the radius of camel no. %d:\n",i+1);
-    scanf("%f",&c[i].height);
-    c[i].weight=0;
+    printf("Enter the height of camel no. %d:\n", i + 1);
+    scanf("%f", &c[i].height);
+    printf("Enter the length of camel no. %d:\n", i + 1);
+    scanf("%f", &c[i].length);
+    printf("Enter the radius of camel no. %d:\n", i + 1);
+    scanf("%f", &c[i].height);
+    c[i].weight = 0;
   }
   printf("Enter the weight of the truck:\n");
-  scanf("%f",truck_weight);
+  scanf("%f", truck_weight);
 }
-void find_camel_weight(int n,Camel c[n])
+void find_camel_weight(int n, Camel c[n])
 {
-  for(int i=0;i<n;i++)
+  for (int i = 0; i < n; i++)
   {
-    const float pi=3.1415;
-    float cu=0;
-    float guess=c[i].height*c[i].length,preguess;
-    while(fabs(guess-preguess)>0.0001)
+    const float pi = 3.1415;
+    float cu = 0;
+    float guess = c[i].height * c[i].length, preguess;
+    while (fabs(guess - preguess) > 0.0001)
     {
-      preguess=guess;
-      guess=0.5*(guess+(c[i].height*c[i].length)/guess);
+      preguess = guess;
+      guess = 0.5 * (guess + (c[i].height * c[i].length) / guess);
     }
-    cu=c[i].radius*c[i].radius*c[i].radius;
-    c[i].weight=pi*cu*guess;
+    cu = c[i].radius * c[i].radius * c[i].radius;
+    c[i].weight = pi * cu * guess;
   }
 }
-float compute_total_weight(int n,Camel c[n],float truck_weight)
+float compute_total_weight(int n, Camel c[n], float truck_weight)
 {
-  float tot_cam_we=0;
-  for(int i=0;i<n;i++)
+  float tot_cam_we = 0;
+  for (int i = 0; i < n; i++)
   {
-    tot_cam_we +=c[i].weight;
+    tot_cam_we += c[i].weight;
   }
-  return truck_weight+tot_cam_we;
+  return truck_weight + tot_cam_we;
 }
 void output(float total_weight)
 {
-  printf("The total weight of the truck is: %.6f",total_weight);
+  printf("The total weight of the truck is: %.6f", total_weight);
 }
 int main()
 {
   int n;
   Camel c[100];
-  float truckweight,total;
-  input(n,c,&truckweight);
-  find_camel_weight(n,c);
-  total=compute_total_weight(n,c,truckweight);
+  float truckweight, total;
+  input(n, c, &truckweight);
+  find_camel_weight(n, c);
+  total = compute_total_weight(n, c, truckweight);
   output(total);
   return 0;
 }
