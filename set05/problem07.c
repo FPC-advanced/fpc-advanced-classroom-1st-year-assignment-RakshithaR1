18fa46b88f5904ce4203a5fd8def356e374cfbd9
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-11-27 10:29:38 +0530

    committed 11/27/2023, 10:29:37 AM

--- set05/problem07.c
+++ set05/problem07.c
@@ -1,36 +1,42 @@
-//Program to find out if the name of the camel is a nice name
-#include<stdio.h>
+// Program to find out if the name of the camel is a nice name
+#include <stdio.h>
 void input(char *name)
 {
   printf("Enter the name of the Camel: ");
-  scanf("%s",name);
+  scanf("%s", name);
 }
 int has_nice_name(char *c)
 {
-  int vov=0,con=0;
-  for(int i=0;c[i]!='\0';i++)
+  int vov = 0, con = 0;
+  for (int i = 0; c[i] != '\0'; i++)
   {
-      
-    if(c[i]=='a'||c[i]=='e'||c[i]=='i'||c[i]=='u'||c[i]=='o'||c[i]=='A'||c[i]=='E'||c[i]=='I'||c[i]=='O'||c[i]=='U')
+
+    if (c[i] == 'a' || c[i] == 'e' || c[i] == 'i' || c[i] == 'u' || c[i] == 'o' || c[i] == 'A' || c[i] == 'E' || c[i] == 'I' || c[i] == 'O' || c[i] == 'U')
     {
-      vov+=1;
-    }else{
-      con +=1;
+      vov += 1;
+    }
+    else
+    {
+      con += 1;
     }
   }
-  if(vov>=2&&con>=2)
+  if (vov >= 2 && con >= 2)
   {
     return 1;
-  }else{
+  }
+  else
+  {
     return 0;
   }
 }
 void output(int res)
 {
-  if(res==1)
+  if (res == 1)
   {
     printf("The camel has a nice name.");
-  }else{
+  }
+  else
+  {
     printf("The camel does not have a nice name.");
   }
 }
@@ -39,7 +45,7 @@ int main()
   char name[100];
   int result;
   input(name);
-  result=has_nice_name(name);
+  result = has_nice_name(name);
   output(result);
   return 0;
 }
