8bc6f1e3277d7557f1b76290f87564c29bf1d099
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-11-27 15:11:37 +0530

    committed 11/27/2023, 3:11:36 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -14,7 +14,7 @@ int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3)
 {
   float area;
   area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
- if (fabs(area)< 0.000001)
+  if (fabs(area) < 0.000001)
   {
     return 0;
   }
