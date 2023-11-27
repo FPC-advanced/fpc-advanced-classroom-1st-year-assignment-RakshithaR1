eabc26d65dc310de27e308bc9bded698080d4eb2
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-11-27 09:48:53 +0530

    committed 11/27/2023, 9:48:52 AM

--- set05/problem02.c
+++ set05/problem02.c
@@ -13,7 +13,8 @@ void input_camel_details(float *radius, float *height, float *length)
 float find_weight(float radius, float height, float length)
 {
   float we, sq;
-  float we=height*
+   we=height*length;
+   float guess=sqrt(we)
   sq = radius * radius * radius;
   return 3.1415 * sq * guess;
 }
