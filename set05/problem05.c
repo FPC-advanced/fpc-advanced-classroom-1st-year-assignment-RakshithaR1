103d7c3647724479d206f7a9ef01bc73374a3e9f
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-11-27 22:17:12 +0530

    committed 11/27/2023, 10:17:11 PM

--- set05/problem05.c
+++ set05/problem05.c
@@ -14,13 +14,7 @@ float borga_x(int x)
    {
      s[i]=pow(x,i);
    }
-   for(int j=0;j<x;j++)
-   {
-     if(j%2!=0)
-     {
-       if(int y=1;y<=j;)
-     }
-   }
+  
 }
 void output(int x,float result)
 {
