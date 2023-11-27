242212d3cc44263a556f1be754e1475f32d46697
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-11-27 09:54:53 +0530

    committed 11/27/2023, 9:54:52 AM

--- set05/problem02.c
+++ set05/problem02.c
@@ -16,7 +16,7 @@ float find_weight(float radius, float height, float length)
   we = height * length;
   float guess = sqrt(we);
   sq = radius * radius * radius;
-  float pi= 22/7;
+  float pi= (float) 22/7;
   return pi * sq * guess;
 }
 void output(float radius, float height, float length, float weight)
