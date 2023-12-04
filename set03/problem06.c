8cc13061f2279c1663a5c1a2ca6adc24a34adfbd
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-12-04 15:27:44 +0530

    committed 12/4/2023, 3:27:44 PM

--- set03/problem06.c
+++ set03/problem06.c
@@ -1,6 +1,6 @@
 // Program to find the index of a substring of a string
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 void input_string(char *a, char *b)
 {
   printf("Enter the string: ");
@@ -8,41 +8,53 @@ void input_string(char *a, char *b)
   printf("Enter the substring whose index has to be found: ");
   scanf("%s", b);
 }
-int sos(char *substring)
+int sub_size(char *substring)
 {
-  int i=0;
-  while(substring[i]!='\0')
+  int i = 0;
+  while (substring[i] != '\0')
   {
     i++;
   }
   return i;
 }
+int str_size(char *string)
+{
+  int j=0;
+  while(string[j]!='\0')
+  {
+    j++;
+  }
+  return j;
+} 
 int sub_str_index(char *string, char *substring)
 {
   int r;
-  for (int j = 0;string[j] != '\0'; j++)
+  int s=str_size(string);
+  int k = sub_size(substring);
+  for (int j = 0; string[j] != '\0'&&string[s]>=substring[k]; j++)
   {
-    
-    if(string[j]==substring[0])
+
+    if (string[j] == substring[0])
     {
-      
-      int k=sos(substring);
-      r=strncmp(string+j,substring,k);
-      if(r==0)
+
+      r = strncmp(string + j, substring, k);
+      if (r == 0)
       {
         return j;
       }
     }
-   }
-   return -1;   
- }
-  
+  }
+  return -1;
+}
+
 void output(char *string, char *substring, int index)
 {
-  if(index>=0)
+  if (index >= 0)
   {
-  printf("The index of '%s' in '%s' is %d", substring, string, index);
-  }else{
+    printf("The index of '%s' in '%s' is %d", substring, string, index);
+  }
+  else
+  {
     printf("The substring does not exist.");
   }
 }
