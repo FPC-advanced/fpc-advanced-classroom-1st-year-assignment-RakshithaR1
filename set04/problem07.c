9a950b65b5ef3ca2fae9e60984da93b24ac205fc
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2023-12-05 09:30:58 +0530

    committed 12/5/2023, 9:30:57 AM

--- set04/problem07.c
+++ set04/problem07.c
@@ -40,10 +40,10 @@ void output(Fraction f1, Fraction f2, Fraction f3, Fraction sum)
 {
   int resnum = f3.num;
   int resden = f3.den;
-  int temp=;
+  int temp=find_gcd(resnum,resden);
  
-  sum.num = f3.num / resnum;
-  sum.den = f3.den / resnum;
+  sum.num = f3.num / temp;
+  sum.den = f3.den / temp;
   printf("%d/%d + %d/%d = %d/%d", f1.num, f1.den, f2.num, f2.den, sum.num, sum.den);
 }
 int main()
